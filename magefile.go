a6b679ac86828d6a959b6698628cb0e5d5043439
weak_ptr <weak_ptr@outlook.com>
2021-12-14 09:44:59 +0800

    build(mage): 清理依赖，改用mage

---
+++ magefile.go
@@ -0,0 +1,23 @@
+//go:build mage
+
+package main
+
+import (
+	"github.com/magefile/mage/sh"
+)
+
+func Build() error {
+	if err := sh.Run("go", "mod", "download"); err != nil {
+		return err
+	}
+
+	if err := sh.Run("go", "mod", "vendor"); err != nil {
+		return err
+	}
+
+	if err := sh.Run("docker-compose", "build"); err != nil {
+		return err
+	}
+
+	return nil
+}
